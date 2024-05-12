e81baa4674983d361931b8abff2d3a04dc820fa0
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-13 02:05:58 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -46,7 +46,7 @@ class $modify(FLAlertLayer) {
     return FLAlertLayer::init(p0, p1, p2, p3, p4, p5, p6, p7, p8);
   }
   void create(FLAlertLayerProtocol *p0, char const *p1, gd::string p2, char const *p3, char const *p4, float p5, bool p6, float p7, float p8) {
-    fr = static_cast<FLAlertLayer*>(this);
+    fr = static_cast<FLAlertLayer*>(FLAlertLayer::create(p0, p1, p2, p3, p4, p5, p6, p7, p8));
     FLAlertLayer::create(p0, p1, p2, p3, p4, p5, p6, p7, p8);
     return;
   }
