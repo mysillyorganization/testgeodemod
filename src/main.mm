5b4b79a6013e6850acfe203160b1b946000ec964
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-13 02:15:00 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -47,12 +47,12 @@ class $modify(FLAlertLayer) {
     return FLAlertLayer::init(p0, p1, p2, p3, p4, p5, p6, p7, p8);
   }
   void create(FLAlertLayerProtocol *p0, char const *p1, gd::string p2, char const *p3, char const *p4, float p5, bool p6, float p7, float p8) {
-    fr = static_cast<FLAlertLayer*>(FLAlertLayer::create(p0, p1, p2, p3, p4, p5, p6, p7, p8));
+    
     FLAlertLayer::create(p0, p1, p2, p3, p4, p5, p6, p7, p8);
     return;
   }
   void show() {
-    
+    fr = static_cast<FLAlertLayer*>(this);
     bool isbtn2 = false;
     NSString* btn2;
     if (m_fields->title) {
