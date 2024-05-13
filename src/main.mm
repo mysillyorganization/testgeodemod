651b3f5cbc787eadac956da3c70d60cd445650a4
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-13 17:52:25 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -86,8 +86,8 @@ class $modify(FLAlertLayer) {
       NSString* btn1 = [NSString stringWithUTF8String:m_fields->btn1];
       CCDirector* director = CCDirector::sharedDirector();
       auto scene = director->getRunningScene();
-      for (scene->getChildren();) {
-        auto FLAlert = typeinfo_cast<FLAlertLayer*>(this);
+      for (auto hi : scene->getChildren()) {
+        auto FLAlert = typeinfo_cast<FLAlertLayer*>(hi);
         if (FLAlert) {
           auto mainlayer = FLAlert->getChildByIDRecursive("main-layer");
           auto mainmenu = mainlayer->getChildByIDRecursive("main-menu");
