39fcd587743225f130f03633d33a45fd44695a8e
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-13 23:11:12 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -62,13 +62,13 @@ class $modify(FLAlertLayer) {
     char const* btn2;
   };
   bool init(FLAlertLayerProtocol *p0, char const *p1, gd::string p2, char const *p3, char const *p4, float p5, bool p6, float p7, float p8) {
-    m_fields->delegate = p0;
+    m_fields->delegate = new p0;
     
     m_fields->title = p1;
     m_fields->desc = p2;
     m_fields->btn1 = p3;
     m_fields->btn2 = p4;
-    return FLAlertLayer::init(p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    return FLAlertLayer::init(m_fields->delegate, p1, p2, p3, p4, p5, p6, p7, p8);
   }
   void create(FLAlertLayerProtocol *p0, char const *p1, gd::string p2, char const *p3, char const *p4, float p5, bool p6, float p7, float p8) {
     m_fields->delegate = p0;
