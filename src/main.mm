06ec0af0041d6a0d2a4a5c9f90df8b6b07e6af94
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-13 20:43:24 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -110,7 +110,7 @@ class $modify(FLAlertLayer) {
       
       //NSLog(@"silly %s", m_fields->delegate);
       //NSLog(@"silly %s", m_fields->fr);
-      showAlert(title, desc, btn1, isbtn2, btn2, m_fields->delegate, alert);
+      showAlert(title, desc, btn1, isbtn2, btn2, m_fields->delegate, m_fields->alert);
       return;
     }
     FLAlertLayer::show();
