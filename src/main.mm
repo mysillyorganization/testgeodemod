1e815f6cc6095a4d16fb5716c8d193392d566c92
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-14 08:04:19 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -46,7 +46,7 @@ void showAlert(NSString* title, NSString* desc, NSString* btn1, bool isbtn2, NSS
 
 class $modify(FLAlertLayerProtocol) {
   void FLAlert_Clicked(FLAlertLayer* flalert, bool btn2) override {
-    NSLog(@"silly the ")
+    NSLog(@"silly the ");
     //FLAlertLayerProtocol::FLAlert_Clicked(flalert, btn2);
     
   }
