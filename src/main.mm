ec2b3fe5b0f1829324a30eab212821e9dc4cf951
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-14 07:24:15 +0300

    Update main.mm

--- src/main.mm
+++ src/main.mm
@@ -78,7 +78,7 @@ class $modify(FLAlertLayer) {
     m_fields->btn2 = p4;
     if (m_fields->title) {
       m_fields->delegate, m_fields->delegate = reinterpret_cast<FLAlertLayerProtocol*>(FLAlertLayer::create(p0, p1, p2, p3, p4, p5, p6, p7, p8));
-      m_fields->alert, m_fields->delegate = reinterpret_cast<FLAlertLayer*>(FLAlertLayer::create(m_fields->delegate, p1, p2, p3, p4, p5, p6, p7, p8));
+      m_fields->alert = reinterpret_cast<FLAlertLayer*>(FLAlertLayer::create(m_fields->delegate, p1, p2, p3, p4, p5, p6, p7, p8));
       //p0->FLAlert_Clicked(m_fields->alert, true);
       return;
     }
