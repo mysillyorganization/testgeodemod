6b78cd46b30c31ff8b62ac17be3cba0c00f4f5d5
pengubow <66965109+pengubow@users.noreply.github.com>
2024-05-14 13:35:58 +0300

    Create hello.hpp

---
+++ src/hello.hpp
@@ -0,0 +1,13 @@
+class testAlertProtocol : public FLAlertLayerProtocol {
+
+public:
+
+	void FLAlert_Clicked(FLAlertLayer* ok, bool btn2) {
+		if (btn2) {
+			FLAlertLayerProtocol::FLAlert_Clicked(ok, btn2);
+		}
+	};
+
+};
+
+#endif
